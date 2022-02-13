503133338fb73dbccfa522c0b2fa7566af4c52ed
Chi Hwan Kim <rlaclghks123@naver.com>
2022-02-13 18:31:28 +0900

    practice - 12 clock, 24clock

--- practice/js/clock.js
+++ practice/js/clock.js
@@ -1,4 +1,5 @@
-const clock = document.querySelector("#clock");
+const clock12 = document.querySelector("#clock12");
+const clock24 = document.querySelector("#clock24");
 const checkbox = document.querySelector(".checkbox");
 
 
@@ -10,7 +11,13 @@ function handleClock() {
     const hour = String(date.getHours()).padStart(2, "0");
     const minute = String(date.getMinutes()).padStart(2, "0");
     const second = String(date.getSeconds()).padStart(2, "0");
-    clock.innerHTML = `${hour} : ${minute} : ${second} `;
+    clock24.innerHTML = `${hour} : ${minute} : ${second} `;
+    if (hour > 12) {
+        clock12.innerHTML = `${hour - 12} : ${minute}  Pm `;
+    } else {
+        clock12.innerHTML = `${hour - 12} : ${minute} : ${second} Am `;
+    }
+
 
 }
 
