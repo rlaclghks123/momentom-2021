db77ee8629b7264449c01c17d44752cb7e038ae1
chiman <rlaclghks123@naver.com>
2023-01-05 20:45:23 +0900

    Fix: API 호출 Async, Await 사용

--- js/weather.js
+++ js/weather.js
@@ -1,27 +1,21 @@
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
 
+async function geoOk(position) {
+  const log = position.coords.longitude;
+  const lat = position.coords.latitude;
+  const API_KEY = "cac17e14d02b1e4d773b1c7a4e02b5eb";
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`;
 
+  const response = await fetch(url);
+  const data = await response.json();
 
-
-function geoOk(position) {
-    const log = position.coords.longitude;
-    const lat = position.coords.latitude;
-    const API_KEY = "cac17e14d02b1e4d773b1c7a4e02b5eb";
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`;
-
-
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            city.innerText = data.name;
-            weather.innerText = `${Math.floor(data.main.temp)}˚C`;
-        });
-
+  city.innerText = data.name;
+  weather.innerText = `${Math.floor(data.main.temp)}˚C`;
 }
 
 function geoError() {
-    console.log("Can`t Fint you. No weather for you");
+  console.log("Can`t Fint you. No weather for you");
 }
 
 navigator.geolocation.getCurrentPosition(geoOk, geoError);
