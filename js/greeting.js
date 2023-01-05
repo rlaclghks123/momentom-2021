42876c1361280455f50544edb597f224aa29af36
chiman <rlaclghks123@naver.com>
2023-01-05 20:46:28 +0900

    Fix: 메모장 기능 수정
    확대, 축소, 닫기, 열기 기능으로 변경

--- js/greeting.js
+++ js/greeting.js
@@ -2,28 +2,27 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#login-h1");
 
-
 const currentUser = "username";
-const HIDDEN="hide";
+const HIDDEN = "hide";
 
-
-function handleSubmit(event){
-    event.preventDefault();
-    loginForm.classList.add(HIDDEN);
-    const currentValue = loginInput.value;
-    localStorage.setItem(currentUser,currentValue);
-    paint(currentValue);
+function handleSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN);
+  const currentValue = loginInput.value;
+  localStorage.setItem(currentUser, currentValue);
+  paint(currentValue);
 }
 
-function paint(username){
-    greeting.innerHTML=`Hello ${username}`;
-    greeting.classList.remove(HIDDEN);
+function paint(username) {
+  greeting.innerHTML = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN);
 }
 
 const savedUser = localStorage.getItem(currentUser);
-if(savedUser===null){
-    loginForm.classList.remove(HIDDEN);
-    loginForm.addEventListener("submit", handleSubmit);
-}else{
-    paint(savedUser);
+
+if (savedUser === null) {
+  loginForm.classList.remove(HIDDEN);
+  loginForm.addEventListener("submit", handleSubmit);
+} else {
+  paint(savedUser);
 }
--- js/toDoList.js
+++ js/toDoList.js
@@ -1,59 +1,84 @@
-const toDoForm = document.querySelector("#toDo-form");
-const toDoInput = toDoForm.querySelector("input");
-const toDo_ul = document.querySelector("#toDo-ul");
-
+const toDoForm = document.querySelector('#toDo-form');
+const toDoInput = toDoForm.querySelector('input');
+const toDo_ul = document.querySelector('#toDo-ul');
+const reactCloseBtn = document.querySelector('.reactBox button:first-child');
+const reactDownSizeBtn = document.querySelector('.reactBox button:nth-child(2)');
+const reactExpandBtn = document.querySelector('.reactBox button:last-child');
+const toDoListBox = document.querySelector('.toDoListBox');
+const toDoListBoxOpenBtn = document.querySelector('#toDoListBoxBtn');
 
 let toDos = [];
 
-const TODO_KEY = "toDos";
+const TODO_KEY = 'toDos';
 
 function saveToDo() {
-    localStorage.setItem(TODO_KEY, JSON.stringify(toDos));
+  localStorage.setItem(TODO_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event) {
-    const li = event.target.parentElement;
-    li.remove();
-    toDos = toDos.filter((item) => item.id != parseInt(li.id));
-    saveToDo();
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((item) => item.id != parseInt(li.id));
+  saveToDo();
 }
 
 function paintToDo(newToDoObj) {
-    const li = document.createElement("li");
-    li.id = newToDoObj.id;
-    const span = document.createElement("span");
-    const button = document.createElement("button");
+  const li = document.createElement('li');
+  li.id = newToDoObj.id;
+  const span = document.createElement('span');
+  const button = document.createElement('button');
 
-    toDo_ul.appendChild(li);
-    li.appendChild(span);
-    span.innerText = newToDoObj.text;
-    li.prepend(button);
-    button.innerText = "❌";
-    button.addEventListener("click", deleteToDo);
+  span.innerText = newToDoObj.text;
+  toDo_ul.appendChild(li);
+  li.appendChild(span);
+  li.append(button);
+  button.innerText = '❌';
+  button.addEventListener('click', deleteToDo);
 }
-
-
 
 function handleSubmit(event) {
-    event.preventDefault();
-    const newToDo = toDoInput.value;
-    toDoInput.value = "";
-    const newToDoObj = {
-        text: newToDo,
-        id: Date.now()
-    };
-    toDos.push(newToDoObj);
-    paintToDo(newToDoObj);
-    saveToDo();
+  event.preventDefault();
+  const newToDo = toDoInput.value;
+  toDoInput.value = '';
+  const newToDoObj = {
+    text: newToDo,
+    id: Date.now(),
+  };
+  toDos.push(newToDoObj);
+  paintToDo(newToDoObj);
+  saveToDo();
 }
 
-toDoForm.addEventListener("submit", handleSubmit);
+function handleCloseBtn(e) {
+  toDoListBox.style.display = 'none';
+  toDoListBox.classList.remove('expand');
+}
+
+function handleOpenBtn(e) {
+  console.log(e);
+  toDoListBox.style.display = 'flex';
+}
+
+function handleDownSizeBtn(e) {
+  toDoListBox.classList.remove('expand');
+}
+
+function handleExpandBtn(e) {
+  toDoListBox.classList.add('expand');
+}
+
+toDoForm.addEventListener('submit', handleSubmit);
+toDoListBoxOpenBtn.addEventListener('click', handleOpenBtn);
+reactCloseBtn.addEventListener('click', handleCloseBtn);
+
+toDoListBoxOpenBtn.addEventListener('click', handleOpenBtn);
+reactDownSizeBtn.addEventListener('click', handleDownSizeBtn);
+reactExpandBtn.addEventListener('click', handleExpandBtn);
 
 const savedToDo = localStorage.getItem(TODO_KEY);
 
 if (savedToDo) {
-    const parseToDo = JSON.parse(savedToDo);
-    toDos = parseToDo;
-    toDos.forEach(paintToDo);
+  const parseToDo = JSON.parse(savedToDo);
+  toDos = parseToDo;
+  toDos.forEach(paintToDo);
 }
-
