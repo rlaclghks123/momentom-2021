bfc0aef14c36df371a91300376b827a991730fce
chiman <rlaclghks123@naver.com>
2023-01-11 21:34:05 +0900

    Feat: clock 오전 오후 기능 추가

--- js/clock.js
+++ js/clock.js
@@ -1,16 +1,15 @@
-const clock = document.querySelector("#clock");
+const clock = document.querySelector('#clock');
 
+function handleClock() {
+  const date = new Date();
 
+  const hour = String(date.getHours()).padStart(2, '0');
+  const minute = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
 
-function handleClock(){   
-
-    const date = new Date();
-    
-    const hour    = String(date.getHours()).padStart(2,"0");  
-    const minute  = String(date.getMinutes()).padStart(2,"0");
-    const seconds = String(date.getSeconds()).padStart(2,"0");
-
-    clock.innerText=`${hour}:${minute}:${seconds}`;
+  hour > 12
+    ? (clock.innerText = `${hour - 12}:${minute}:${seconds} PM`)
+    : (clock.innerHTML = `${hour} : ${minute} : ${seconds} AM`);
 }
 
 handleClock();
